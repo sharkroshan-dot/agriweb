b6caec655ca99decb18614905b577280d6a97f63
sharkroshan-dot <sharkroshan@gmail.com>
2026-09-23 18:04:34 +0530

    Remove redundant space below sticky header

--- website/app/(customer)/layout.tsx
+++ website/app/(customer)/layout.tsx
@@ -34,7 +34,7 @@ export default function CustomerLayout({ children }: { children: React.ReactNode
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
-      <div className="flex flex-1 bg-transparent pt-16">
+      <div className="flex flex-1 bg-transparent">
         <CustomerSidebar />
         <main className="site-main min-w-0 flex-1 overflow-x-hidden px-3 py-5 sm:px-5 sm:py-6 lg:px-8">
           <div className="site-content mx-auto w-full max-w-[1440px]">{children}</div>
