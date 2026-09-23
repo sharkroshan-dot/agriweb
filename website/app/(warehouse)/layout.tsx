c4ca8bfd5b1bb08681b701759de594bd90db9d2f
sharkroshan-dot <sharkroshan@gmail.com>
2026-09-23 17:20:09 +0530

    Polish warehouse page layout spacing

--- website/app/(warehouse)/layout.tsx
+++ website/app/(warehouse)/layout.tsx
@@ -37,8 +37,8 @@ export default function WarehouseLayout({
       <Header />
       <div className="flex flex-1 bg-transparent pt-16">
         <WarehouseSidebar />
-        <main className="min-w-0 flex-1 overflow-x-hidden px-3 py-5 sm:px-5 sm:py-6 lg:px-8">
-          <div className="mx-auto w-full max-w-[1440px]">{children}</div>
+        <main className="site-main min-w-0 flex-1 overflow-x-hidden px-3 py-5 sm:px-5 sm:py-6 lg:px-8">
+          <div className="site-content mx-auto w-full max-w-[1440px]">{children}</div>
         </main>
       </div>
       <Footer />
